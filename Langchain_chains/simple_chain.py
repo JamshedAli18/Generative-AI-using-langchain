0c54a8202b9bd5cc94db1cd28f7128e050024e76
Jamshed Ali <116933185+JamshedAli18@users.noreply.github.com>
2025-08-31 12:55:02 +0500

    Implement simple chain for text summarization

---
+++ Langchain_chains/simple_chain.py
@@ -0,0 +1,27 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+prompt = PromptTemplate(
+    template = "Write a summary of {topic}",
+    input_variables=['topic']
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({"topic": "cricket"})
+
+# print(result)
+chain.get_graph().print_ascii()
